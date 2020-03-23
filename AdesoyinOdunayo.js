94ad2dc15dd8277056a766b4fda870659425d770
Adesoyin odunayo <61968605+Ballerina20@users.noreply.github.com>
2020-03-23 20:27:10 +0100

    Add files via upload

---
+++ AdesoyinOdunayo.js
@@ -0,0 +1,14 @@
+let Name = "Adesoyin Odunayo";
+
+let Courses = new Array("HTML", "CSS", "JavaScript");
+
+console.log("My Name is:"  +  Name);
+console.log("My Courses are:"  +  Courses);
+let i=1;
+for(let i=1; i<=200; i++)
+{
+if (i%2!==0) {
+console.log(i  + " is an odd number");
+
+}
+}
